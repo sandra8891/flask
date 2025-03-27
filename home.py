7628828f0ff4a1834590c13f2a8ef51d843f20dc
sandra8891@ <sandraks8891@gmal.com>
2025-03-27 16:06:35 +0530

    mysql connect

--- home.py
+++ home.py
@@ -1,10 +1,19 @@
 from flask import Flask,render_template,request,redirect
-import sqlite3
+# import sqlite3
 App=Flask(__name__)
+import mysql.connector
 
+# con=sqlite3.connect("database.db")
+# con.execute("CREATE TABLE IF NOT EXISTS users(name TEXT,age int)")
 
-con=sqlite3.connect("database.db")
-con.execute("CREATE TABLE IF NOT EXISTS users(name TEXT,age int)")
+con = mysql.connector.connect(
+    host="localhost",
+    user="sandra",
+    password="sandra",
+    database="sandra"
+)
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS user(name TEXT,age int)")
 
 @App.route("/",methods=['GET','POST'])
 def index():
@@ -12,8 +21,10 @@ def index():
         name=request.form['name']
         age=request.form['age']
         print(name,age)
-        con=sqlite3.connect("database.db")
-        con.execute("insert nto users(name,age) values(?,?)",(name,age))
+        cur = con.cursor()
+        cur.execute("INSERT INTO user(name,age) VALUES(%s,%s)",(name,age))
+        # con=sqlite3.connect("database.db")
+        # con.execute("insert into users(name,age) values(?,?)",(name,age))
         con.commit()
         con.close()
         return redirect("/")
